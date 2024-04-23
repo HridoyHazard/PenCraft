9437045542c141760bfcdd46db57a2f8038000be
HridoyHazard <hridoyhazard47@gmail.com>
2024-04-24 02:59:20 +0600

    update

--- frontend/src/screens/CreateArticle.jsx
+++ frontend/src/screens/CreateArticle.jsx
@@ -68,7 +68,7 @@ const CreateArticle = () => {
   return (
     <div class="max-w-xl mx-auto mt-20">
       <Link
-        to={"/dashboard/dashboard/articlelist"}
+        to={"/dashboard/articlelist"}
         type="button"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-800 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -52,7 +52,7 @@ const Register = () => {
           id="back-div"
           class="bg-gradient-to-r from-blue-500 to-purple-500 rounded-[26px] m-4"
         >
-          <div class="border-[20px] border-transparent rounded-[20px] dark:bg-gray-900 bg-white shadow-lg xl:p-10 2xl:p-10 lg:p-10 md:p-10 sm:p-2 m-2">
+          <div class="border-transparent rounded-[20px] dark:bg-gray-900 bg-white shadow-lg xl:p-10 2xl:p-10 lg:p-10 md:p-10 sm:p-2 m-2">
             <h1 class="pt-8 pb-6 font-bold dark:text-gray-400 text-5xl text-center cursor-default">
               Sign Up
             </h1>
